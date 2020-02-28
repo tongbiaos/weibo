41ee87584e10566f6881c2bd99240e98bf0a3a3e
“tbs” <tongbiaos@163.com>
2020-02-28 09:47:31 +0000

    切割头部和尾部子视图

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+  <img class="brand-icon" src="http://myimg.tb1043.com/logo_245c204e7512642e5c66675d6a8c0db7.png"/>
+  <a href="https://laravel-china.org/courses" target=_blank>
+    刻意练习，每日精进
+  </a>
+
+  <div class="float-right">
+    <a href="/about" >关于</a>
+  </div>
+</footer>
