7ab7d54725e7eabb9283c1a103b8375ecfa21886
tongbiaos <tongbiaos@163.com>
2020-02-29 21:28:28 +0800

    test2

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>@yield('title', 'Weibo AppKSS') - TBS</title>
+    <title>@yield('title', 'Weibo App AX') - TBS111</title>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
   </head>
   <body>
@@ -9,6 +9,7 @@
 
     <div class="container">
       <div class="offset-md-1 col-md-10">
+        <p>12345</p>
         @include('shared._messages')
         @yield('content')
         @include('layouts._footer')
